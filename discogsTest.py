8d38b1490e2bb84fc97b331e8f04b0f91fdb1f60
Mattias Soderberg <mattiassoderberj@gmail.com>
2018-06-07 16:48:33 +0200

    more tesing
    playing around with tracks

--- discogsTest.py
+++ discogsTest.py
@@ -9,25 +9,31 @@ title_in = raw_input('What Title? ').title()
 res_out = d.search(group_in, type='master', artist=group_in, release_title=title_in)
 res_out2 = d.search(title_in, artist=group_in, type='release')
 
-print len(res_out2)
+print len(res_out)
+for i in res_out:
+    print i
 
-print res_out.pages
-extract = res_out.page(1)[0]
-print extract
-print dir(extract)
+def prin_trx(trx):
+    ret_trx = []
+    for i in trx:
+        ret_trx.append(i)
+    return ret_trx
 
 
-artist = res_out2[0].artists[1]
+if res_out.pages >=1:
+    extract = res_out.page(1)[0]
+    extr_01 = extract.main_release
+    extr_02 = extract.tracklist
+    print extr_01
+    print prin_trx(extr_02)
+    #print dir(extract)
+else:
+    print "No pages found"
+    exit()
+
+artist = res_out2[0].artists[0]
 print len(res_out2[0].artists)
 print artist.name
 
 
 print title_in
-
-for i in res_out.page(1):
-    row = i.title.split('-')
-    if row[1].strip() == title_in:
-        print row[0].strip(), row[1].strip()
-        cln_trk = str(i.tracklist[0]).replace('<','').replace('>','').split('u')
-        print str(cln_trk[2]).strip('\'')
-        print i.id
